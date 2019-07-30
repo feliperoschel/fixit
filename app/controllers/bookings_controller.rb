c71a0db9e9621eabe507393cf7e5092ebe691c8d
steven ly <shiwenly@gmail.com>
2019-07-30 20:35:00 +0800

    create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,19 +11,42 @@ class BookingsController < ApplicationController
   end
 
   def new
-
+    @user = current_user
+    @booking = Booking.new
+    authorize @booking
+    @painting = Painting.find(params[:painting_id])
   end
 
   def create
-
+    @user = current_user
+    @booking = Booking.new(booking_params)
+    @booking.user = @user
+    @painting = Painting.find(params[:painting_id])
+    @booking.painting = @painting
+    authorize @booking
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      raise
+    end
   end
 
   def edit
-
+    @user = current_user
+    @booking = Booking.find(params[:id])
   end
 
   def update
+    @user = current_user
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
   end
 
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :location, :time)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :paintings do
     resources :reviews, only: [:new, :create]
+    resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:new, :create, :edit, :update, :show, :index]
+  resources :bookings, only: [:edit, :update, :show, :index]
   root to: 'pages#home'
 
 end
