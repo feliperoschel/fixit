caa960e30724dc8dba3f22c25ac98ddaf1505eac
steven ly <shiwenly@gmail.com>
2019-07-30 16:24:43 +0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 require 'faker'
 
 User.delete_all if Rails.env.development?
-# Painting.delete_all if Rails.env.development?
-# User.delete_all if Rails.env.development?
-# Review.delete_all if Rails.env.development?
+Painting.delete_all if Rails.env.development?
+User.delete_all if Rails.env.development?
+Review.delete_all if Rails.env.development?
 
 
 20.times do
@@ -24,43 +24,43 @@ User.delete_all if Rails.env.development?
   user.save
 end
 
-# User.all.each do |user|
-#  painting = Painting.new(
-#   title: Faker::Book.title,
-#   description: Faker::Commerce.product_name,
-#   category: Faker::Book.genre,
-#   price: Faker::Commerce.price ,
-#   photo: "" ,
-#   location: Faker::Address.city,
-#   user: user)
-#   painting.save
-# end
+User.all.each do |user|
+ painting = Painting.new(
+  title: Faker::Book.title,
+  description: Faker::Commerce.product_name,
+  category: Faker::Book.genre,
+  price: Faker::Commerce.price ,
+  photo: "" ,
+  location: Faker::Address.city,
+  user: user)
+  painting.save
+end
 
-# 20.times do
-#   user = User.all.sample
-#   painting = Painting.all.sample
-#   booking = Booking.new(
-#     start_date: Faker::Date.forward(23),
-#     end_date: :start_date + rand(1..10),
-#     total_price: (:end_date - :start_date) * painting.price,
-#     location: Faker::Address.full_address,
-#     time: rand(9..18),
-#     user: user,
-#     painting: painting
-#     )
-# end
+20.times do
+  user = User.all.sample
+  painting = Painting.all.sample
+  booking = Booking.new(
+    start_date: Faker::Date.forward(23),
+    end_date: :start_date + rand(1..10),
+    total_price: (:end_date - :start_date) * painting.price,
+    location: Faker::Address.full_address,
+    time: rand(9..18),
+    user: user,
+    painting: painting
+    )
+end
 
-# Booking.all.each do |booking|
-#   rand(3..5).times do
-#     Booking.new(
-#       booking: booking,
-#       date: Faker::Date.forward(23),
-#       title: Faker::Lorem.sentence,
-#       content: Faker::Lorem.sentences,
-#       rating: rand(0..5)
-#     )
-#   end
-# end
+Painting.all.each do |painting|
+  rand(3..5).times do
+    Review.new(
+      painting: painting,
+      date: Faker::Date.forward(23),
+      title: Faker::Lorem.sentence,
+      content: Faker::Lorem.sentences,
+      rating: rand(0..5)
+    )
+  end
+end
 
 
 
