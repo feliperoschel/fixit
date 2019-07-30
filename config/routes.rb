8c347685b1055fc2889fa19e2d3b484a5e6e9678
steven chareonbood <stoodsnob@gmail.com>
2019-07-30 17:59:23 +0800

    modifying gitignore and pages show and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
+
+  # # ORIGINAL
+  # devise_for :users
+  # root to: 'pages#home'
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #   resources :paintings
+  #   resources :bookings do
+  #     resources :reviews, only: [:new, :create]
+  #   end
+  # end
+
+  # TRYOUT
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :paintings do
+    resources :review, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:new, :create, :edit, :update, :show, :index]
+
+
 end
