0ff4afbeab95b1ef820a33ab1e0d8d9b89015e76
steven chareonbood <stoodsnob@gmail.com>
2019-07-30 18:59:14 +0800

    pulled app.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-
-  # # ORIGINAL
-  # devise_for :users
-  # root to: 'pages#home'
-  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #   resources :paintings
-  #   resources :bookings do
-  #     resources :reviews, only: [:new, :create]
-  #   end
-  # end
-
-  # TRYOUT
-  devise_for :users
-
-  resources :paintings do
-    resources :review, only: [:new, :create]
-  end
-
-  resources :bookings, only: [:new, :create, :edit, :update, :show, :index]
-
-
-=======
-  get 'reviews/create'
-  get 'reviews/new'
-  devise_for :users
-  resources :paintings do
-    resources :reviews, only: [:new, :create]
-  end
-  resources :bookings, only: [:new, :create, :edit, :update, :show, :index]
-  root to: 'pages#home'
->>>>>>> master
+ devise_for :users
+ resources :paintings do
+   resources :reviews, only: [:new, :create]
+ end
+ resources :bookings, only: [:new, :create, :edit, :update, :show, :index]
+ root to: 'pages#home'
 end
