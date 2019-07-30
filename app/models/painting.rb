2a30a619cdaf44995031bf6023621750363c9c22
steven chareonbood <stoodsnob@gmail.com>
2019-07-30 13:01:41 +0800

    added painting model

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,3 +1,9 @@
 class Painting < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :price, presence: true
+  validates :title, presence: true
+
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_033720) do
+ActiveRecord::Schema.define(version: 2019_07_30_040847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "paintings", force: :cascade do |t|
+    t.text "title"
+    t.text "description"
+    t.text "category"
+    t.integer "price"
+    t.string "photo"
+    t.string "location"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_paintings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +42,5 @@ ActiveRecord::Schema.define(version: 2019_07_30_033720) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "paintings", "users"
 end
