aba3271c2667eefca43d2c83f210970fcf5ac4d0
Richard Sugianto So <scyrs1@nottingham.edu.cn>
2019-07-31 13:37:33 +0800

    conflict

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -3,10 +3,6 @@ class Painting < ApplicationRecord
   validates :description, presence: true, length: {maximum: 300}
   validates :category, :price, presence: true
   belongs_to :user
-<<<<<<< HEAD
-  has_many :reviews
-=======
   has_many :reviews, dependent: :destroy
->>>>>>> master
   has_many :bookings
 end
