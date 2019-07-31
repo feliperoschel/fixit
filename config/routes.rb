9b60634dc51b7fad2dbafeba098f489353c2f1d8
steven chareonbood <stoodsnob@gmail.com>
2019-07-31 11:07:34 +0800

    styling home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< HEAD
- devise_for :users
- resources :paintings do
-   resources :reviews, only: [:new, :create]
- end
- resources :bookings, only: [:new, :create, :edit, :update, :show, :index]
- root to: 'pages#home'
-=======
-=======
-
->>>>>>> master
   devise_for :users
   resources :paintings do
     resources :reviews, only: [:new, :create]
   end
   resources :bookings, only: [:new, :create, :edit, :update, :show, :index]
   root to: 'pages#home'
-<<<<<<< HEAD
->>>>>>> master
-=======
-
->>>>>>> master
 end
