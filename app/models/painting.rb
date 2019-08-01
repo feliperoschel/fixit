4c8d37f94095c4111c7b5b1b455ca0289a9e2d22
Felipe Roschel <f.roschel@gmail.com>
2019-08-01 14:47:44 +0800

    date validation on bookings model and price validation on painting model

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -2,6 +2,7 @@ class Painting < ApplicationRecord
   validates :title, presence: true, length: {maximum: 50}
   validates :description, presence: true, length: {maximum: 300}
   validates :category, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
   belongs_to :user
   has_many :reviews, dependent: :destroy
   has_many :bookings
