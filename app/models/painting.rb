52bc13091be5c3bedef5540f2e9085c4012b15a2
Richard Sugianto So <scyrs1@nottingham.edu.cn>
2019-08-01 15:55:42 +0800

    recover missing files

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,7 +1,7 @@
 class Painting < ApplicationRecord
   validates :title, presence: true, length: {maximum: 50}
   validates :description, presence: true, length: {maximum: 300}
-  validates :category, :price, presence: true
+  validates :category, :price, :photo, presence: true
   validates :price, numericality: { greater_than: 0 }
   belongs_to :user
   has_many :reviews, dependent: :destroy
